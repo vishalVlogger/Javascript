41a593772533ac71523206140e6d07dc36bbf3fb
Vishal <vishal.patil362000@gmail.com>
2024-02-16 19:21:00 +0530

    Recaptcha Changes

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ function beforeSubmit() {
   let formattedDate = new Date(inputDate.value).toLocaleDateString("en-US");
   outputDate.value = formattedDate;
 }
+
+function timestamp() {
+  var response = document.getElementById("g-recaptcha-response");
+  if (response == null || response.value.trim() == "") {
+    var elems = JSON.parse(
+      document.getElementsByName("captcha_settings")[0].value
+    );
+    elems["ts"] = JSON.stringify(new Date().getTime());
+    document.getElementsByName("captcha_settings")[0].value =
+      JSON.stringify(elems);
+  }
+}
+setInterval(timestamp, 500);
