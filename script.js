b901b73a5aa9bc1534ca74faeb37ce7f6783d10c
Vishal <vishal.patil362000@gmail.com>
2024-02-16 19:33:26 +0530

    reCAPTCHA Validation Added

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-function beforeSubmit() {
-  let outputDate = document.querySelector(".outputDate");
-  let inputDate = document.querySelector(".inputDate");
-  console.log("Value: ", inputDate.value);
+let captchaCheecked = false;
 
-  let formattedDate = new Date(inputDate.value).toLocaleDateString("en-US");
-  outputDate.value = formattedDate;
+function beforeSubmit(event) {
+  if (captchaCheecked) {
+    let outputDate = document.querySelector(".outputDate");
+    let inputDate = document.querySelector(".inputDate");
+    console.log("Value: ", inputDate.value);
+
+    let formattedDate = new Date(inputDate.value).toLocaleDateString("en-US");
+    outputDate.value = formattedDate;
+  } else {
+    alert("Please check the reCAPTCHA box to submit the lead");
+    event.preventDefault();
+  }
 }
 
 function timestamp() {
@@ -19,3 +26,7 @@ function timestamp() {
   }
 }
 setInterval(timestamp, 500);
+
+function captchacall() {
+  captchaCheecked = true;
+}
